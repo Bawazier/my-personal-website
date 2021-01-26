a2c046fb340eda2e56bd3b74abf832ec1cd1c42a
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-01-26 16:33:08 +0700

    create portfolio and about section

--- components/layout.js
+++ components/layout.js
@@ -4,26 +4,40 @@ export const siteTitle = "M."
 
 function layout({children}) {
     return (
-        <div className="container">
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <header className="flex flex-row mx-auto px-20 py-4 justify-between items-center w-screen">
-                <div>
-                    <h1 className="font-sans text-5xl font-black text-yellow-500 uppercase">{siteTitle}</h1>
-                </div>
-                <div className="flex space-x-6">
-                    <a href="#" className="font-sans">Home</a>
-                    <a href="#aboutMe" className="font-sans">About Us</a>
-                    <a href="#" className="font-sans">Services</a>
-                    <a href="#" className="font-sans">Portfolio</a>
-                    <a href="#" className="font-sans">News</a>
-                    <a href="#" className="font-sans">Contact</a>
-                </div>
-            </header>
-            <main className="flex flex-col mx-auto px-20 py-4 w-screen">{children}</main>
-        </div>
-    )
+      <div className="container mx-auto">
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <header className="flex flex-row mx-auto py-4 justify-between items-center w-4/5">
+          <div>
+            <h1 className="font-sans text-5xl font-black text-yellow-500 uppercase">
+              {siteTitle}
+            </h1>
+          </div>
+          <div className="flex space-x-6">
+            <a href="#" className="font-sans">
+              Home
+            </a>
+            <a href="#aboutMe" className="font-sans">
+              About Us
+            </a>
+            <a href="#services" className="font-sans">
+              Services
+            </a>
+            <a href="#" className="font-sans">
+              Portfolio
+            </a>
+            <a href="#" className="font-sans">
+              News
+            </a>
+            <a href="#" className="font-sans">
+              Contact
+            </a>
+          </div>
+        </header>
+        <main className="flex flex-col mx-auto py-4 w-4/5">{children}</main>
+      </div>
+    );
 }
 
 export default layout
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout';
 import styles from '../styles/Home.module.css'
+import { FaReact, FaGithub, FaAndroid } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -12,54 +13,181 @@ export default function Home() {
       <section className="w-full max-h-screen flex items-center justify-between">
         <div className="flex space-y-10 flex-col w-full">
           <div className="flex flex-col space-y-1.5">
-            <h4 className="font-sans text-lg font-semibold text-gray-400 uppercase">Hello</h4>
-            <h1 className="font-serif text-6xl font-bold text-black capitalize">I'm Michael</h1>
-            <p className="font-sans text-2xl font-semibold text-gray-600 capitalize">Freelance Web & Mobile Application</p>
+            <h4 className="font-sans text-lg font-semibold text-gray-400 uppercase">
+              Hello
+            </h4>
+            <h1 className="font-serif text-6xl font-bold text-black capitalize">
+              I'm Michael
+            </h1>
+            <p className="font-sans text-2xl font-semibold text-gray-600 capitalize">
+              Freelance Web & Mobile Application
+            </p>
           </div>
           <div>
-            <button className="text-white text-base rounded-lg bg-yellow-500 font-bold font-sans text-center w-52 h-14">HIRE ME</button>
+            <button className="text-white text-base rounded-lg bg-yellow-500 font-bold font-sans text-center w-52 h-14">
+              HIRE ME
+            </button>
           </div>
         </div>
         <div className="w-full h-auto">
           <img src="/images/person.png" className="w-auto h-full" />
         </div>
       </section>
-      <section id="aboutMe" className="w-full h-screen flex items-center justify-between">
+      <section
+        id="aboutMe"
+        className="w-full h-screen flex items-center justify-between"
+      >
         <div className="w-full">
           <div className=""></div>
           <div>
-            <h1 className="font-serif text-4xl font-bold text-black capitalize">About Me</h1>
+            <h1 className="font-serif text-4xl font-bold text-black capitalize">
+              About Me
+            </h1>
             <hr className="w-14 h-1 bg-yellow-500" />
           </div>
         </div>
         <div className="w-full flex flex-col space-y-6">
           <div>
-            <p className="leading-relaxed break-words text-gray-600">However Emmet completions will not show up for the case you showed: the string "lorem5".
-            The reason is that emmet sees the closing " immediately after the abbreviation so it decides not to return any completions.
-            Instead, you have to use the Emmet: Expand Abbreviation command (which is bound to tab by default) to force emmet to expand lorem5:
-            However Emmet completions will not show up for the case you showed: the string "lorem5".</p>&nbsp;&nbsp;
-            <p className="leading-relaxed break-words text-gray-600">The reason is that emmet sees the closing " immediately after the abbreviation so it decides not to return any completions.
-          Instead, you have to use the Emmet: Expand Abbreviation command (which is bound to tab by default) to force emmet to expand lorem5:</p>
+            <p className="leading-relaxed break-words text-gray-600">
+              However Emmet completions will not show up for the case you
+              showed: the string "lorem5". The reason is that emmet sees the
+              closing " immediately after the abbreviation so it decides not to
+              return any completions. Instead, you have to use the Emmet: Expand
+              Abbreviation command (which is bound to tab by default) to force
+              emmet to expand lorem5: However Emmet completions will not show up
+              for the case you showed: the string "lorem5".
+            </p>
+            &nbsp;&nbsp;
+            <p className="leading-relaxed break-words text-gray-600">
+              The reason is that emmet sees the closing " immediately after the
+              abbreviation so it decides not to return any completions. Instead,
+              you have to use the Emmet: Expand Abbreviation command (which is
+              bound to tab by default) to force emmet to expand lorem5:
+            </p>
           </div>
           <div className="flex space-x-4">
-            <button className="text-white text-base rounded-lg bg-yellow-500 font-bold font-sans text-center w-52 h-14">VIEW WORKS</button>
-            <button className="text-yellow-500 text-base rounded-lg border-2 border-yellow-500 font-bold font-sans text-center w-52 h-14">DOWNLOAD CV</button>
-         </div>
+            <button className="text-white text-base rounded-lg bg-yellow-500 font-bold font-sans text-center w-52 h-14">
+              VIEW WORKS
+            </button>
+            <button className="text-yellow-500 text-base rounded-lg border-2 border-yellow-500 font-bold font-sans text-center w-52 h-14">
+              DOWNLOAD CV
+            </button>
+          </div>
         </div>
       </section>
-      <section id="services" className="w-full h-screen flex flex-col items-center justify-center">
+      <section
+        id="services"
+        className="w-full h-screen flex flex-col space-y-6 items-center justify-center"
+      >
         <div className="flex flex-col space-y-4 items-center justify-center">
-          <h1 className="font-serif text-4xl font-bold text-black capitalize">Services</h1>
+          <h1 className="font-serif text-4xl font-bold text-black capitalize">
+            Services
+          </h1>
           <hr className="w-20 h-1 bg-yellow-500" />
-          <p className="leading-relaxed break-words text-gray-600 text-center">The reason is that emmet sees the closing " immediately after the abbreviation so it decides not to return any completions.
-          Instead, you have to use the Emmet: Expand Abbreviation command (which is bound to tab by default) to force emmet to expand lorem5:</p>
+          <p className="leading-relaxed break-words text-gray-600 text-center">
+            The reason is that emmet sees the closing " immediately after the
+            abbreviation so it decides not to return any completions. Instead,
+            you have to use the Emmet: Expand Abbreviation command (which is
+            bound to tab by default) to force emmet to expand lorem5:
+          </p>
         </div>
-        <div>
-          <div>
-            
+        <div className="grid grid-cols-3 gap-4">
+          <div className="bg-yellow-500 shadow-lg rounded-md p-8 flex flex-col space-y-3.5">
+            <FaReact className="w-10 h-10 text-red-400" />
+            <h1 className="font-sans text-xl font-bold text-white">
+              Web Development
+            </h1>
+            <p className="text-gray-500 whitespace-normal">
+              The reason is that emmet sees the closing " immediately after the
+              abbreviation so it decides not to return any completions.
+            </p>
           </div>
+          <div className="bg-yellow-500 shadow-lg rounded-md p-8 flex flex-col space-y-3.5">
+            <FaGithub className="w-10 h-10 text-red-400" />
+            <h1 className="font-sans text-xl font-bold text-white">
+              Web Development
+            </h1>
+            <p className="text-gray-500 whitespace-normal">
+              The reason is that emmet sees the closing " immediately after the
+              abbreviation so it decides not to return any completions.
+            </p>
+          </div>
+          <div className="bg-yellow-500 shadow-lg rounded-md p-8 flex flex-col space-y-3.5">
+            <FaAndroid className="w-10 h-10 text-red-400" />
+            <h1 className="font-sans text-xl font-bold text-white">
+              Web Development
+            </h1>
+            <p className="text-gray-500 whitespace-normal">
+              The reason is that emmet sees the closing " immediately after the
+              abbreviation so it decides not to return any completions.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section
+        id="portfolio"
+        className="w-full h-screen flex flex-col space-y-6 items-center justify-center"
+      >
+        <div className="flex flex-col space-y-4 items-center justify-center">
+          <h1 className="font-serif text-4xl font-bold text-black capitalize">
+            Portfolio
+          </h1>
+          <hr className="w-20 h-1 bg-yellow-500" />
+          <p className="leading-relaxed break-words text-gray-600 text-center">
+            The reason is that emmet sees the closing " immediately after the
+            abbreviation so it decides not to return any completions. Instead,
+            you have to use the Emmet: Expand Abbreviation command (which is
+            bound to tab by default) to force emmet to expand lorem5:
+          </p>
+        </div>
+        <div className="">
+          <div className="w-full bg-yellow-500 grid grid-cols-2 gap-4">
+            <div></div>
+            <div>
+              <h1 className="font-sans text-xl font-bold text-white">
+                Send Bae
+              </h1>
+              <p className="text-gray-500 whitespace-normal">
+                Send Bae is a messaging application that connects people through
+                a unique set of data centers equipped with realtime message
+                features, 20+ dialing codes, and Images Messages designed with
+                React Native.
+              </p>
+              <div className="flex space-x-4">
+                <div>
+                  <h4>Repository :</h4>
+                  <a href="http://bit.ly/SendBae_git">
+                    http://bit.ly/SendBae_git
+                  </a>
+                </div>
+                <div>
+                  <h4>Visi Project :</h4>
+                  <a href="#http://bit.ly/SendBae_apk">
+                    http://bit.ly/SendBae_apk
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        id="contact"
+        className="w-full h-screen flex flex-col space-y-6 items-center justify-center"
+      >
+        <div className="flex flex-col space-y-4 items-center justify-center">
+          <h1 className="font-serif text-4xl font-bold text-black capitalize">
+            Contact Us
+          </h1>
+          <hr className="w-20 h-1 bg-yellow-500" />
+          <p className="leading-relaxed break-words text-gray-600 text-center">
+            The reason is that emmet sees the closing " immediately after the
+            abbreviation so it decides not to return any completions. Instead,
+            you have to use the Emmet: Expand Abbreviation command (which is
+            bound to tab by default) to force emmet to expand lorem5:
+          </p>
         </div>
       </section>
     </Layout>
-  )
+  );
 }
